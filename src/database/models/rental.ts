e9dec1df0ce49307757a4775aa9126dcea3e3530
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-08 10:54:32 -0500

    Add rental model schema definition

---
+++ src/database/models/rental.ts
@@ -0,0 +1,25 @@
+import mongoose, { Model, Schema, model } from 'mongoose';
+import { IRental } from '../../interfaces';
+
+const rentalSchema = new Schema({
+  client:    { type: String, required: true, default: 'NN' },
+  time:      { type: Number, required: true },
+  date:      { type: Date, required: true },
+  vehicle:   { type: Schema.Types.ObjectId, ref: 'Vehicle', required: true },
+  payment:   { type: String, required: true,
+               enum: {
+                values: ['cash', 'nequi', 'bancolombia', 'daviplata'],
+                message: '{VALUE} no es un medio de pago válido',
+                default: 'cash',
+                required: true
+               }
+             },
+  amount:    { type: Number, required: true },
+  exception: { type: String, required: false }
+}, {
+  versionKey: false
+});
+
+const Rental: Model<IRental> = mongoose.models.Rental || model<IRental>('Rental', rentalSchema);
+
+export default Rental;
