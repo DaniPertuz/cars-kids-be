ee414fddf2670ff651923f9560f0cfa3ce25d787
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-08 11:01:09 -0500

    Add repository abstract class for user

---
+++ src/domain/repository/user.repository.ts
@@ -0,0 +1,9 @@
+import { IUserRole } from '../../interfaces';
+import { UserEntity } from '../entities/user.entity';
+
+export abstract class UserRepository {
+  // Admin actions
+  abstract getUsers(): Promise<UserEntity[]>;
+  abstract updateUserRole(email: string, role: IUserRole): Promise<UserEntity | null>;
+  abstract deactivateUser(email: string): Promise<void>;
+}
