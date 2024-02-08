caeb7ab86b5f70022b0f37f62e1318371f912eb8
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-08 10:54:23 -0500

    Add user model schema definition

---
+++ src/database/models/user.ts
@@ -0,0 +1,31 @@
+import mongoose, { Model, Schema, model } from 'mongoose';
+import { IUser } from '../../interfaces';
+
+const userSchema = new Schema({
+  name:     { type: String, required: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  status:   { type: String,
+              enum: {
+                values: ['active', 'inactive'],
+                message: '{VALUE} no es un estado válido',
+                default: 'active',
+                required: true
+              }
+            },
+  role:     { type: String,
+              enum: {
+                values: ['admin', 'editor'],
+                message: '{VALUE} no es un rol válido',
+                default: 'editor',
+                required: true
+              }
+            }
+}, {
+  timestamps: true,
+  versionKey: false
+});
+
+const User: Model<IUser> = mongoose.models.User || model<IUser>('User', userSchema);
+
+export default User;
