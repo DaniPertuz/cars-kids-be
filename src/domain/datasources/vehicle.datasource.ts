eb0586001a74389bae7bac2f2ee67a86c54f28e0
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-08 11:00:05 -0500

    Add datasource abstract class for vehicle

---
+++ src/domain/datasources/vehicle.datasource.ts
@@ -0,0 +1,15 @@
+import { IStatus, IVehicle, IVehicleSize } from '../../interfaces';
+import { VehicleEntity } from '../entities/vehicle.entity';
+
+export abstract class VehicleDatasource {
+  abstract createVehicle(vehicle: VehicleEntity): Promise<VehicleEntity>;
+  abstract getVehicleById(id: string): Promise<VehicleEntity | null>;
+  abstract getVehicleByNickname(nickname: string): Promise<VehicleEntity | null>;
+  abstract getVehicles(): Promise<VehicleEntity[]>;
+  abstract getVehiclesByColor(color: string): Promise<VehicleEntity[]>;
+  abstract getVehiclesByColorAndSize(color: string, size: IVehicleSize): Promise<VehicleEntity[]>;
+  abstract getVehiclesBySize(size: IVehicleSize): Promise<VehicleEntity[]>;
+  abstract getVehiclesByStatus(status: IStatus): Promise<VehicleEntity[]>;
+  abstract updateVehicle(nickname: string, vehicle: VehicleEntity): Promise<VehicleEntity | null>;
+  abstract deactivateVehicle(nickname: string): Promise<void>;
+}
