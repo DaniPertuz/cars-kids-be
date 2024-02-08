87dcafd2eb07820d9cb6469629d92ae0d4011ec8
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-08 11:03:21 -0500

    Implement RentalDTO for handling rental data validation

---
+++ src/domain/dtos/rental/index.ts
@@ -0,0 +1,26 @@
+import { IRental } from '../../../interfaces';
+
+export class RentalDTO {
+  private constructor(public params: IRental) { }
+
+  static create(object: IRental): [string?, RentalDTO?] {
+    const { client, time, date, vehicle, payment, amount } = object;
+
+    if (!client) return ['Nombre de cliente es requerido'];
+    if (!time) return ['Tiempo es requerido'];
+    if (!date) return ['Fecha es requerida'];
+    if (!vehicle) return ['Vehículo es requerido'];
+    if (!payment) return ['Tipo de pago es requerido'];
+    if (!amount) return ['Monto es requerido'];
+
+    return [undefined, new RentalDTO(object)];
+  }
+
+  static update(object: IRental): [string?, RentalDTO?] {
+    const { _id, client, time, date, vehicle, payment, amount } = object;
+
+    if (!_id) return ['ID de alquiler no es válido'];
+
+    return [undefined, new RentalDTO({ _id, client, time, date, vehicle, payment, amount })];
+  }
+}
