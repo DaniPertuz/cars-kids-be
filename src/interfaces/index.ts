715913a2303e1151d6321b9e3f6cc02db79b6faf
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-08 10:52:10 -0500

    App interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,63 @@
+import { Router } from 'express';
+
+export enum ICategory {
+  Car   = 'car',
+  Cycle = 'cycle'
+}
+
+export enum IPayment {
+  Cash        = 'cash',
+  Nequi       = 'nequi',
+  Bancolombia = 'bancolombia',
+  Daviplata   = 'daviplata'
+}
+
+export enum IStatus {
+  Active   = 'active',
+  Inactive = 'inactive'
+}
+
+export enum IUserRole {
+  Admin  = 'admin',
+  Editor = 'editor'
+}
+
+export enum IVehicleSize {
+  Small  = 'S',
+  Medium = 'M',
+  Large  = 'L'
+}
+
+export interface IRental {
+  _id?:       string;
+  client:     string;
+  time:       number;
+  date:       string;
+  vehicle:    string;
+  payment:    IPayment;
+  amount:     number;
+  exception?: string; 
+}
+
+export interface IUser {
+  email:    string;
+  password: string;
+  name:     string;
+  role:     IUserRole;
+  status:   IStatus;
+}
+
+export interface IVehicle {
+  nickname: string;
+  category: ICategory;
+  color:    string;
+  img?:     string;
+  size:     IVehicleSize;
+  status:   IStatus;
+}
+
+export interface ServerOptions {
+  port:         number;
+  public_path?: string;
+  routes:       Router;
+}
