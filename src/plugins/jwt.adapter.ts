29919c5ad9bb83f40abb70af94a09553a5d50b85
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-08 10:50:28 -0500

    jwt config

---
+++ src/plugins/jwt.adapter.ts
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { envs } from './envs.plugin';
+
+const JWT_SEED = envs.JWT_SEED;
+
+export class JwtAdapter {
+
+  static async generateJWT(payload: any, duration: string = '8h') {
+    return new Promise((resolve) => {
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
+        if (err) return resolve(null);
+
+        resolve(token);
+      });
+    });
+  }
+
+  static validateToken(token: string) {
+    return new Promise((resolve) => {
+      jwt.verify(token, JWT_SEED, (err, decoded) => {
+        if (err) return resolve(null);
+
+        resolve(decoded);
+      });
+    });
+  }
+
+}
