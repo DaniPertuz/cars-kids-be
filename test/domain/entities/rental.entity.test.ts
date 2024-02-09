1dc11d0f405d6a347a317ad04733fc0cecd3062e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-09 14:37:40 -0500

    Testing on RentalEntity

---
+++ test/domain/entities/rental.entity.test.ts
@@ -0,0 +1,39 @@
+import { RentalEntity } from '../../../src/domain/entities/rental.entity';
+import { IPayment } from '../../../src/interfaces';
+
+describe('Rental entity', () => {
+  const data = {
+    client: 'Test Name',
+    time: 15,
+    date: '01-01-2020',
+    vehicle: 'd3ba2daad17250e579833f0e',
+    payment: IPayment.Cash,
+    amount: 10000,
+    exception: 'Test exception'
+  };
+
+  test('should create a RentalEntity instance', () => {
+    const user = new RentalEntity(data);
+
+    expect(user).toBeInstanceOf(RentalEntity);
+    expect(user.params.client).toBe(data.client);
+    expect(user.params.time).toBe(data.time);
+    expect(user.params.date).toBe(data.date);
+    expect(user.params.vehicle).toBe(data.vehicle);
+    expect(user.params.payment).toBe(data.payment);
+    expect(user.params.amount).toBe(data.amount);
+    expect(user.params.exception).toBe(data.exception);
+  });
+
+  test('should create a VehicleEntity from object', () => {
+    const user = RentalEntity.fromObject(data);
+    
+    expect(user).toBeInstanceOf(RentalEntity);
+    expect(typeof user.params.time).toBe(Number);
+    expect(typeof user.params.date).toBe(String);
+    expect(typeof user.params.vehicle).toBe(String);
+    expect(typeof user.params.payment).toBe(String);
+    expect(typeof user.params.amount).toBe(Number);
+    expect(typeof user.params.exception).toBe(String);
+  });
+});
