6f01cb20647e8de86372d7dd393e63a5f87be0e6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-09 14:37:55 -0500

    Testing on VehicleEntity

---
+++ test/domain/entities/vehicle.entity.test.ts
@@ -0,0 +1,37 @@
+import { VehicleEntity } from '../../../src/domain/entities/vehicle.entity';
+import { ICategory, IStatus, IVehicleSize } from '../../../src/interfaces';
+
+describe('Vehicle entity', () => {
+  const data = {
+    nickname: 'Test Name',
+    img: 'Test image',
+    category: ICategory.Car,
+    color: '#000000',
+    size: IVehicleSize.Large,
+    status: IStatus.Active
+  };
+
+  test('should create a VehicleEntity instance', () => {
+    const vehicle = new VehicleEntity(data);
+
+    expect(vehicle).toBeInstanceOf(VehicleEntity);
+    expect(vehicle.params.nickname).toBe(data.nickname);
+    expect(vehicle.params.img).toBe(data.img);
+    expect(vehicle.params.category).toBe(data.category);
+    expect(vehicle.params.color).toBe(data.color);
+    expect(vehicle.params.size).toBe(data.size);
+    expect(vehicle.params.status).toBe(data.status);
+  });
+
+  test('should create a VehicleEntity from object', () => {
+    const vehicle = VehicleEntity.fromObject(data);
+    
+    expect(vehicle).toBeInstanceOf(VehicleEntity);
+    expect(vehicle.params.nickname).toBe('Test Name');
+    expect(vehicle.params.img).toBe('Test image');
+    expect(vehicle.params.category).toBe(ICategory.Car);
+    expect(vehicle.params.color).toBe('#000000');
+    expect(vehicle.params.size).toBe(IVehicleSize.Large);
+    expect(vehicle.params.status).toBe(IStatus.Active);
+  });
+});
