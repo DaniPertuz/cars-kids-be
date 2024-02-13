3c92c5642d4d58697b93b59ff27d0f380a8b4c21
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-13 18:15:20 -0500

    Testing on User repository implementation

---
+++ test/domain/infrastructure/repositories/user-impl.repository.test.ts
@@ -0,0 +1,34 @@
+import { IUserRole } from '../../../../src/interfaces';
+import { UserDatasource } from '../../../../src/domain/datasources/user.datasource';
+import { UserRepositoryImpl } from '../../../../src/infrastructure/repositories/user-impl.repository';
+
+type MockUserDatasource = UserDatasource & {
+  [key: string]: jest.Mock<any, any>;
+};
+
+describe('User repository implementation', () => {
+  const mockUserDatasource: MockUserDatasource = {
+    getUsers: jest.fn(),
+    updateUserRole: jest.fn(),
+    deactivateUser: jest.fn()
+  };
+
+  const userRepositoryImpl = new UserRepositoryImpl(mockUserDatasource);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const testCases = [
+    { method: 'getUsers', args: [], },
+    { method: 'updateUserRole', args: ['test@test.com', IUserRole.Editor] },
+    { method: 'deactivateUser', args: ['test@test.com'] }
+  ];
+
+  testCases.forEach(({ method, args }) => {
+    test(`should call ${method} on the datasource`, async () => {
+      await (userRepositoryImpl as any)[method](...args);
+      expect(mockUserDatasource[method]).toHaveBeenCalled();
+    });
+  });
+});
