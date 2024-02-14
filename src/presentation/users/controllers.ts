95ced1df439af6e2f18fb066fedc5c07e6838edb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-14 01:42:40 -0500

    Update user validation before data handling

--- src/presentation/users/controllers.ts
+++ src/presentation/users/controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { MongoUserDatasource } from '../../infrastructure/datasources/mongo-user.datasource';
 import { UserRepositoryImpl } from '../../infrastructure/repositories/user-impl.repository';
 import { IStatus, IUserRole } from '../../interfaces';
+import User from '../../database/models/user';
 
 export class UsersController {
   readonly userRepo = new UserRepositoryImpl(
@@ -17,9 +18,7 @@ export class UsersController {
   public updateUserRole = async (req: Request, res: Response) => {
     const { email, role } = req.body;
 
-    if (!(Object.values(IUserRole).includes(role))) {
-      return res.status(400).json({ error: 'Rol de usuario no válido' });
-    }
+    if (!(Object.values(IUserRole).includes(role))) return res.status(400).json({ error: 'Rol de usuario no válido' });
 
     const user = await this.userRepo.updateUserRole(email, role as IUserRole);
 
@@ -28,6 +27,10 @@ export class UsersController {
 
   public deactivateUser = async (req: Request, res: Response) => {
     const { email } = req.body;
+
+    const userDB = await User.findOne({ email });
+
+    if (!userDB) return res.status(404).json({ error: 'Usuario no encontrado' });
 
     this.userRepo.deactivateUser(email);
 
