fa2cb1b74e4cf4579b7a625ed325ba759a8b0986
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-14 01:41:45 -0500

    Add serverListener variable

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -5,6 +5,7 @@ import { ServerOptions } from '../interfaces';
 
 export class Server {
   public readonly app = express();
+  private serverListener?: any;
   private readonly port: number;
   private readonly publicPath: string;
   private readonly routes: Router;
@@ -40,8 +41,12 @@ export class Server {
       res.sendFile(indexPath);
     });
 
-    this.app.listen(this.port, () => {
-      console.log(`Server runnning on port ${this.port}`);
+    this.serverListener = this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
     });
+  }
+
+  public close() {
+    this.serverListener?.close();
   }
 }
