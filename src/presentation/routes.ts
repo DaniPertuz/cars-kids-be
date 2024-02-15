f2554687c6847c0030c11054a3bd7cc64281d84a
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-15 00:43:21 -0500

    Add ProductsRoutes to AppRoutes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AuthRoutes } from './auth/routes';
+import { ProductsRoutes } from './products/routes';
 import { RentalsRoutes } from './rentals/routes';
 import { UsersRoutes } from './users/routes';
 import { VehiclesRoutes } from './vehicles/routes';
@@ -9,6 +10,7 @@ export class AppRoutes {
     const router = Router();
 
     router.use('/api/auth', AuthRoutes.routes);
+    router.use('/api/products', ProductsRoutes.routes);
     router.use('/api/rentals', RentalsRoutes.routes);
     router.use('/api/users', UsersRoutes.routes);
     router.use('/api/vehicles', VehiclesRoutes.routes);
