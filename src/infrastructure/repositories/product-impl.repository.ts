9f420cce2f6b07e29105f2ce65f15e4c1acbd452
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-15 00:41:45 -0500

    Implementation ProductRepositoryImpl

---
+++ src/infrastructure/repositories/product-impl.repository.ts
@@ -0,0 +1,27 @@
+import { ProductDatasource } from '../../domain/datasources/product.datasource';
+import { ProductEntity } from '../../domain/entities/product.entity';
+import { ProductRepository } from '../../domain/repository/product.repository';
+
+export class ProductRepositoryImpl implements ProductRepository {
+  constructor(private readonly productDatasource: ProductDatasource) { }
+
+  createProduct(product: ProductEntity): Promise<ProductEntity> {
+    return this.productDatasource.createProduct(product);
+  }
+
+  getProducts(): Promise<ProductEntity[]> {
+    return this.productDatasource.getProducts();
+  }
+
+  getProduct(name: string): Promise<ProductEntity | null> {
+    return this.productDatasource.getProduct(name);
+  }
+
+  updateProduct(name: string, product: ProductEntity): Promise<ProductEntity | null> {
+    return this.productDatasource.updateProduct(name, product);
+  }
+
+  deleteProduct(name: string): Promise<ProductEntity | null> {
+    return this.productDatasource.deleteProduct(name);
+  }  
+}
