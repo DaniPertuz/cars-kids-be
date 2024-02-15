50aa1e9e4b947e665a223965149e82d4fa430d47
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-15 00:41:12 -0500

    Abstract class ProductRepository

---
+++ src/domain/repository/product.repository.ts
@@ -0,0 +1,9 @@
+import { ProductEntity } from '../entities/product.entity';
+
+export abstract class ProductRepository {
+  abstract createProduct(product: ProductEntity): Promise<ProductEntity>;
+  abstract getProducts(): Promise<ProductEntity[]>;
+  abstract getProduct(name: string): Promise<ProductEntity | null>;
+  abstract updateProduct(name: string, product: ProductEntity): Promise<ProductEntity | null>;
+  abstract deleteProduct(name: string): Promise<ProductEntity | null>;
+}
