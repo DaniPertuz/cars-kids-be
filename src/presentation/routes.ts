dd9b67f5834e6fdda02d7beb681ce36b114b5ed1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-15 14:05:06 -0500

    Add PurchasesRoutes to AppRoutes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -4,12 +4,14 @@ import { ProductsRoutes } from './products/routes';
 import { RentalsRoutes } from './rentals/routes';
 import { UsersRoutes } from './users/routes';
 import { VehiclesRoutes } from './vehicles/routes';
+import { PurchasesRoutes } from './purchase/routes';
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.use('/api/auth', AuthRoutes.routes);
+    router.use('/api/purchases', PurchasesRoutes.routes);
     router.use('/api/products', ProductsRoutes.routes);
     router.use('/api/rentals', RentalsRoutes.routes);
     router.use('/api/users', UsersRoutes.routes);
