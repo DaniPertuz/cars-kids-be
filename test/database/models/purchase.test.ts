cf999c473e54bc9fbb077769b52319d390067cc3
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-15 21:21:47 -0500

    Testing Purchase model

---
+++ test/database/models/purchase.test.ts
@@ -0,0 +1,53 @@
+import { Schema } from 'mongoose';
+import { connect, disconnect } from '../../../src/database';
+import { ProductModel, PurchaseModel } from '../../../src/database/models';
+import { IStatus } from '../../../src/interfaces';
+
+describe('Purchase model', () => {
+  beforeAll(async () => {
+    await connect();
+  });
+
+  afterAll(async () => {
+    await disconnect();
+  });
+
+  test('should return PurchaseModel', async () => {
+    const productData = {
+      name: 'Test Product',
+      amount: 10000,
+      status: IStatus.Active
+    };
+
+    const product = await ProductModel.create(productData);
+
+    const productId = product._id;
+    
+    const purchaseData = {
+      product: productId,
+      quantity: 1,
+      price: 10000,
+      purchaseDate: new Date('2000-11-10T05:00:00.000Z')
+    };
+
+    const purchase = await PurchaseModel.create(purchaseData);
+
+    expect(purchase.toJSON()).toEqual(expect.objectContaining(purchaseData));
+
+    await PurchaseModel.findOneAndDelete({ purchaseDate: purchase.purchaseDate });
+    await ProductModel.findByIdAndDelete(productId);
+  });
+
+  test('should return the schema object', () => {
+    const expectedSchema = {
+      product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+      quantity: { type: Number, required: true },
+      price: { type: Number, required: true },
+      purchaseDate: { type: Date, required: true }
+    };
+
+    const schema = PurchaseModel.schema.obj;
+
+    expect(schema).toEqual(expectedSchema);
+  });
+});
