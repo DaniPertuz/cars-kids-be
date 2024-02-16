d5e123b9a9a8f3617888c4f1cd9730cc47d2d8f1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-15 21:21:30 -0500

    Testing Product model

---
+++ test/database/models/product.test.ts
@@ -0,0 +1,47 @@
+import { connect, disconnect } from '../../../src/database';
+import { IStatus } from '../../../src/interfaces';
+import { ProductModel } from '../../../src/database/models';
+
+describe('Product model', () => {
+  beforeAll(async () => {
+    await connect();
+  });
+
+  afterAll(async () => {
+    await disconnect();
+  });
+
+  test('should return ProductModel', async () => {
+    const productData = {
+      name: 'Test Name',
+      amount: 10000,
+      status: IStatus.Active
+    };
+
+    const product = await ProductModel.create(productData);
+
+    expect(product.toJSON()).toEqual(expect.objectContaining(productData));
+
+    await ProductModel.findOneAndDelete({ name: product.name });
+  });
+
+  test('should return the schema object', () => {
+    const expectedSchema = {
+      name: { type: expect.any(Function), required: true },
+      amount: { type: expect.any(Function), required: true },
+      status: {
+        type: expect.any(Function),
+        enum: {
+          values: ['active', 'inactive'],
+          message: '{VALUE} no es un estado válido',
+          default: 'active',
+          required: true
+        }
+      }
+    };
+
+    const schema = ProductModel.schema.obj;
+
+    expect(schema).toEqual(expect.objectContaining(expectedSchema));
+  });
+});
