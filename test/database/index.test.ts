2dba24f0ced2c774688f46fbae1c71270d630af5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-15 22:46:52 -0500

    Remove comments

--- test/database/index.test.ts
+++ test/database/index.test.ts
@@ -22,13 +22,13 @@ describe('Connection to MongoDB', () => {
 
   test('should not connect to MongoDB when a connection already exists', async () => {
     await connect();
-    expect(mongoose.connect).not.toHaveBeenCalled(); // Verificar que no se haya llamado a mongoose.connect
+    expect(mongoose.connect).not.toHaveBeenCalled();
   });
 
   test('should disconnect and then connect to MongoDB when a connection exists but is not ready', async () => {
     await disconnect();
     await connect();
-    expect(mongoose.disconnect).toHaveBeenCalled(); // Verificar que se haya llamado a mongoose.disconnect
-    expect(mongoose.connect).toHaveBeenCalled(); // Verificar que se haya llamado a mongoose.connect
+    expect(mongoose.disconnect).toHaveBeenCalled();
+    expect(mongoose.connect).toHaveBeenCalled();
   });
 });
