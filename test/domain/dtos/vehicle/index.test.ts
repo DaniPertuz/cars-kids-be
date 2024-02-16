73da69005ecd877dbd381549c753e9458567360e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-16 01:45:14 -0500

    Testing on VehicleDTO

---
+++ test/domain/dtos/vehicle/index.test.ts
@@ -0,0 +1,86 @@
+import { ICategory, IStatus, IVehicle, IVehicleSize } from '../../../../src/interfaces';
+import { VehicleDTO } from '../../../../src/domain/dtos/vehicle/index';
+
+describe('VehicleDTO', () => {
+  const validObject: IVehicle = {
+    nickname: 'Test Vehicle',
+    category: ICategory.Car,
+    color: '#000000',
+    size: IVehicleSize.Large,
+    status: IStatus.Active
+  };
+
+  describe('create', () => {
+    test('should return error when nickname field is missing', () => {
+      const invalidObject = {
+        category: ICategory.Car,
+        color: '#000000',
+        size: IVehicleSize.Large,
+        status: IStatus.Active
+      };
+      const [error, vehicleDTO] = VehicleDTO.create(invalidObject);
+
+      expect(error).toBe('Apodo de vehículo es requerido');
+      expect(vehicleDTO).toBeUndefined();
+    });
+
+    test('should return error when category field is missing', () => {
+      const invalidObject = {
+        nickname: 'Test Vehicle',
+        color: '#000000',
+        size: IVehicleSize.Large,
+        status: IStatus.Active
+      };
+      const [error, vehicleDTO] = VehicleDTO.create(invalidObject);
+
+      expect(error).toBe('Categoría de vehículo es requerida');
+      expect(vehicleDTO).toBeUndefined();
+    });
+
+    test('should return error when color field is missing', () => {
+      const invalidObject = {
+        nickname: 'Test Vehicle',
+        category: ICategory.Car,
+        size: IVehicleSize.Large,
+        status: IStatus.Active
+      };
+      const [error, vehicleDTO] = VehicleDTO.create(invalidObject);
+
+      expect(error).toBe('Color de vehículo es requerido');
+      expect(vehicleDTO).toBeUndefined();
+    });
+
+    test('should return error when size field is missing', () => {
+      const invalidObject = {
+        nickname: 'Test Vehicle',
+        category: ICategory.Car,
+        color: '#000000',
+        status: IStatus.Active
+      };
+      const [error, vehicleDTO] = VehicleDTO.create(invalidObject);
+
+      expect(error).toBe('Tamaño de vehículo es requerido');
+      expect(vehicleDTO).toBeUndefined();
+    });
+
+    test('should return error when status field is missing', () => {
+      const invalidObject = {
+        nickname: 'Test Vehicle',
+        category: ICategory.Car,
+        color: '#000000',
+        size: IVehicleSize.Large
+      };
+      const [error, vehicleDTO] = VehicleDTO.create(invalidObject);
+
+      expect(error).toBe('Estado de vehículo es requerido');
+      expect(vehicleDTO).toBeUndefined();
+    });
+
+    test('should return ProductDTO instance when object is valid', () => {
+      const [error, vehicleDTO] = VehicleDTO.create(validObject);
+
+      expect(error).toBeUndefined();
+      expect(vehicleDTO).toBeInstanceOf(VehicleDTO);
+    });
+  });
+});
