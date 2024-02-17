f988832e6d11da72b258163daee199f1eb02b4ee
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-16 21:19:14 -0500

    Testing on Product repository implementation

---
+++ test/domain/infrastructure/repositories/product-impl.repository.test.ts
@@ -0,0 +1,47 @@
+import { IStatus } from '../../../../src/interfaces';
+import { ProductDatasource } from '../../../../src/domain/datasources/product.datasource';
+import { ProductEntity } from '../../../../src/domain/entities/product.entity';
+import { ProductRepositoryImpl } from '../../../../src/infrastructure/repositories/product-impl.repository';
+
+type MockProductDatasource = ProductDatasource & {
+  [key: string]: jest.Mock<any, any>;
+};
+
+describe('Product repository implementation', () => {
+  const mockProductDatasource: MockProductDatasource = {
+    createProduct: jest.fn(),
+    getProduct: jest.fn(),
+    getActiveProducts: jest.fn(),
+    getAllProducts: jest.fn(),
+    updateProduct: jest.fn(),
+    deleteProduct: jest.fn()
+  };
+
+  const productRepositoryImpl = new ProductRepositoryImpl(mockProductDatasource);
+
+  const product = new ProductEntity({
+    name: 'Test Product',
+    price: 10000,
+    status: IStatus.Active
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const testCases = [
+    { method: 'createProduct', args: [product], },
+    { method: 'getProduct', args: [product] },
+    { method: 'getActiveProducts', args: [] },
+    { method: 'getAllProducts', args: [] },
+    { method: 'updateProduct', args: ['d34a2daad17250e579833f0e'] },
+    { method: 'deleteProduct', args: ['d34a2daad17250e579833f0e'] },
+  ];
+
+  testCases.forEach(({ method, args }) => {
+    test(`should call ${method} on the datasource`, async () => {
+      await (productRepositoryImpl as any)[method](...args);
+      expect(mockProductDatasource[method]).toHaveBeenCalled();
+    });
+  });
+});
