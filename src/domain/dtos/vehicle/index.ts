57d10cdeae3462211d9db2960fea6b83a80b3eab
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-22 12:49:45 -0500

    Update vehicle status validation

--- src/domain/dtos/vehicle/index.ts
+++ src/domain/dtos/vehicle/index.ts
@@ -1,16 +1,15 @@
-import { IVehicle } from '../../../interfaces';
+import { IStatus, IVehicle } from '../../../interfaces';
 
 export class VehicleDTO {
   private constructor(public params: IVehicle) { }
 
   static create(object: { [key: string]: any; }): [string?, VehicleDTO?] {
-    const { nickname, category, color, size, status } = object;
+    const { nickname, category, color, size, status = IStatus.Active } = object;
 
     if (!nickname) return ['Apodo de vehículo es requerido'];
     if (!category) return ['Categoría de vehículo es requerida'];
     if (!color) return ['Color de vehículo es requerido'];
     if (!size) return ['Tamaño de vehículo es requerido'];
-    if (!status) return ['Estado de vehículo es requerido'];
 
     return [undefined, new VehicleDTO({ nickname, category, color, size, status })];
   }
