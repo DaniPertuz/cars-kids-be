777054c649b1c2738a00a4e2b094190d1ee20137
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-22 19:05:27 -0500

    Set default value for status prop

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -1,19 +1,18 @@
-import { IUser } from '../../../interfaces';
+import { IStatus, IUser } from '../../../interfaces';
 import { regularExps } from '../../../utils/regularExps';
 
 export class RegisterUserDTO {
   private constructor(public params: IUser) { }
 
   static create(object: IUser): [string?, RegisterUserDTO?] {
-    const { name, email, password, role, status } = object;
+    const { name, email, password, role, status = IStatus.Active } = object;
 
     if (!email) return ['Email es requerido'];
     if (!regularExps.email.test(email)) return ['Email no es válido'];
     if (!password) return ['Contraseña es requerida'];
     if (!name) return ['Nombre es requerido'];
     if (!role) return ['Rol es requerido'];
-    if (!status) return ['Estado es requerido'];
 
-    return [undefined, new RegisterUserDTO(object)];
+    return [undefined, new RegisterUserDTO({ name, email, password, role, status })];
   }
 }
