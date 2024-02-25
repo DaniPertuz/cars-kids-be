2ce3bc83a465b752bd4037c27fbcb62ef000b666
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-25 16:24:33 -0500

    Add user prop to validation

--- src/domain/dtos/purchase/index.ts
+++ src/domain/dtos/purchase/index.ts
@@ -4,21 +4,22 @@ export class PurchaseDTO {
   private constructor(public params: IPurchase) { }
 
   static create(object: { [key: string]: any; }): [string?, PurchaseDTO?] {
-    const { _id, price, product, purchaseDate, quantity } = object;
+    const { _id, price, product, purchaseDate, quantity, user } = object;
 
     if (!price) return ['Precio de compra es requerido'];
     if (!product) return ['ID de producto es requerido'];
     if (!purchaseDate) return ['Fecha de compra es requerida'];
     if (!quantity) return ['Cantidad de items comprados es requerida'];
+    if (!user) return ['Usuario es requerido'];
 
-    return [undefined, new PurchaseDTO({ _id, price, product, purchaseDate, quantity })];
+    return [undefined, new PurchaseDTO({ _id, price, product, purchaseDate, quantity, user })];
   }
 
   static update(object: { [key: string]: any; }): [string?, PurchaseDTO?] {
-    const { _id, price, product, purchaseDate, quantity } = object;
+    const { _id, price, product, purchaseDate, quantity, user } = object;
 
     if (!_id) return ['ID de compra es requerido'];
 
-    return [undefined, new PurchaseDTO({ _id, price, product, purchaseDate, quantity })];
+    return [undefined, new PurchaseDTO({ _id, price, product, purchaseDate, quantity, user })];
   }
 }
--- src/domain/dtos/rental/index.ts
+++ src/domain/dtos/rental/index.ts
@@ -4,7 +4,7 @@ export class RentalDTO {
   private constructor(public params: IRental) { }
 
   static create(object: { [key: string]: any; }): [string?, RentalDTO?] {
-    const { client, time, date, vehicle, payment, amount } = object;
+    const { client, time, date, vehicle, payment, amount, user } = object;
 
     if (!client) return ['Nombre de cliente es requerido'];
     if (!time) return ['Tiempo es requerido'];
@@ -12,15 +12,16 @@ export class RentalDTO {
     if (!vehicle) return ['Vehículo es requerido'];
     if (!payment) return ['Tipo de pago es requerido'];
     if (!amount) return ['Monto es requerido'];
+    if (!user) return ['Usuario es requerido'];
 
-    return [undefined, new RentalDTO({ client, time, date, vehicle, payment, amount })];
+    return [undefined, new RentalDTO({ client, time, date, vehicle, payment, amount, user })];
   }
 
   static update(object: IRental): [string?, RentalDTO?] {
-    const { _id, client, time, date, vehicle, payment, amount } = object;
+    const { _id, client, time, date, vehicle, payment, amount, user } = object;
 
     if (!_id) return ['ID de alquiler no es válido'];
 
-    return [undefined, new RentalDTO({ _id, client, time, date, vehicle, payment, amount })];
+    return [undefined, new RentalDTO({ _id, client, time, date, vehicle, payment, amount, user })];
   }
 }
