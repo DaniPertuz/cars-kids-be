613652d49a8a5d737737802f7023c0aeae0507ef
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-27 21:56:48 -0500

    Remove validations

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,20 +1,11 @@
 import { IUser } from '../../interfaces';
-import { regularExps } from '../../utils/regularExps';
-import { CustomError } from '../errors';
 
 export class UserEntity {
   constructor(public params: IUser) { }
 
   static fromObject = (object: IUser): UserEntity => {
     const { email, password, name, role, status } = object;
-
-    if (!email) throw CustomError.badRequest('Email es requerido');
-    if (!regularExps.email.test(email)) throw CustomError.badRequest('Email no es válido');
-    if (!name) throw CustomError.badRequest('Nombre es requerido');
-    if (!role) throw CustomError.badRequest('Rol es requerido');
-
-    const userEntityParams: IUser = { email, password, name, role, status };
-    const user = new UserEntity(userEntityParams);
+    const user = new UserEntity({ email, password, name, role, status });
     return user;
   };
 }
