ec15f87ae7308f2750e9f7157a0c9f5a3b389063
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-10 01:12:08 -0500

    Add index file for entities

---
+++ src/domain/entities/index.ts
@@ -0,0 +1,6 @@
+export * from './budget.entity';
+export * from './product.entity';
+export * from './purchase.entity';
+export * from './rental.entity';
+export * from './user.entity';
+export * from './vehicle.entity';
