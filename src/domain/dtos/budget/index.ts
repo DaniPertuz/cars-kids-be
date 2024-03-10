0fd3d494347acccb7c2aa23aa45b1dc088a1ca1c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-10 01:48:21 -0500

    BudgetDTO

---
+++ src/domain/dtos/budget/index.ts
@@ -0,0 +1,14 @@
+import { IBudget } from '../../../interfaces';
+
+export class BudgetDTO {
+  private constructor(public params: IBudget) { }
+
+  static create(object: { [key: string]: any; }): [string?, BudgetDTO?] {
+    const { base, expenses = 0, loans = 0, date } = object;
+
+    if (!base) return ['Base es requerida'];
+    if (!date) return ['Fecha de presupuesto es requerida'];
+
+    return [undefined, new BudgetDTO({ base, expenses, loans, date })];
+  }
+}
