862a09423cd598f87cb7c88469a8f8f9bdee98ee
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-11 17:32:12 -0500

    Testing on BudgetDTO

---
+++ test/domain/dtos/budget/index.test.ts
@@ -0,0 +1,40 @@
+import { BudgetDTO } from '../../../../src/domain/dtos/budget/index';
+import { IBudget } from '../../../../src/interfaces';
+
+describe('BudgetDTO', () => {
+  const validObject: IBudget = {
+    base: 100000,
+    expenses: 0,
+    loans: 0,
+    date: new Date().toISOString()
+  };
+
+  describe('create', () => {
+    test('should return error when base field is missing', () => {
+      const invalidObject = {
+        date: new Date().toISOString()
+      };
+      const [error, budgetDTO] = BudgetDTO.create(invalidObject);
+
+      expect(error).toBe('Base es requerida');
+      expect(budgetDTO).toBeUndefined();
+    });
+
+    test('should return error when date field is missing', () => {
+      const invalidObject = {
+        base: 100000
+      };
+      const [error, budgetDTO] = BudgetDTO.create(invalidObject);
+
+      expect(error).toBe('Fecha de presupuesto es requerida');
+      expect(budgetDTO).toBeUndefined();
+    });
+
+    test('should return BudgetDTO instance when object is valid', () => {
+      const [error, budgetDTO] = BudgetDTO.create(validObject);
+
+      expect(error).toBeUndefined();
+      expect(budgetDTO).toBeInstanceOf(BudgetDTO);
+    });
+  });
+});
