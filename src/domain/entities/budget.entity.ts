686dee37d68faa276078a208cfb3e81f854a2833
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-04-04 12:50:11 -0500

    Include _id on budget object

--- src/domain/entities/budget.entity.ts
+++ src/domain/entities/budget.entity.ts
@@ -4,7 +4,7 @@ export class BudgetEntity {
   constructor(public params: IBudget) { }
 
   static fromObject = (object: IBudget): BudgetEntity => {
-    const { base, expenses, loans, date } = object;
-    return new BudgetEntity({ base, expenses, loans, date });
+    const { _id, base, expenses, loans, date } = object;
+    return new BudgetEntity({ _id, base, expenses, loans, date });
   };
 }
