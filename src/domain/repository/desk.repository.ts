7efa50eae0ecf6e8c82235466b0543db13824a5a
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-01 23:33:52 -0500

    Desk entity CRUD methods

---
+++ src/domain/repository/desk.repository.ts
@@ -0,0 +1,11 @@
+import { DeskQueryResult } from '../../interfaces';
+import { PaginationDto } from '../dtos/shared/pagination.dto';
+import { DeskEntity } from '../entities/desk.entity';
+
+export abstract class DeskRepository {
+  abstract createDesk(desk: DeskEntity): Promise<DeskEntity>;
+  abstract getDesks(paginationDto: PaginationDto): Promise<DeskQueryResult>;
+  abstract getDesk(name: string): Promise<DeskEntity | null>;
+  abstract updateDesk(name: string, desk: DeskEntity): Promise<DeskEntity | null>;
+  abstract deleteDesk(name: string): Promise<DeskEntity | null>;
+}
