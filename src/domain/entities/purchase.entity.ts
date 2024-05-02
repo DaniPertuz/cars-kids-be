83415d6c72856206108988811ccbc7dd7fd5cd63
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-05-02 00:13:22 -0500

    Add desk prop to entity

--- src/domain/entities/purchase.entity.ts
+++ src/domain/entities/purchase.entity.ts
@@ -4,7 +4,7 @@ export class PurchaseEntity {
   constructor(public params: IPurchase) { }
 
   static fromObject = (object: IPurchase): PurchaseEntity => {
-    const { _id, price, product, payment, purchaseDate, quantity, user } = object;
-    return new PurchaseEntity({ _id, product, quantity, price, payment, purchaseDate, user });
+    const { _id, price, product, payment, purchaseDate, quantity, user, desk } = object;
+    return new PurchaseEntity({ _id, product, quantity, price, payment, purchaseDate, user, desk });
   };
 }
--- src/domain/entities/rental.entity.ts
+++ src/domain/entities/rental.entity.ts
@@ -4,8 +4,8 @@ export class RentalEntity {
   constructor(public params: IRental) { }
 
   static fromObject = (object: { [key: string]: any; }): RentalEntity => {
-    const { _id, client, time, date, vehicle, payment, amount, user, exception } = object;
-    const rentalEntityParams: IRental = { _id, client, time, date, vehicle, payment, amount, user, exception };
+    const { _id, client, time, date, vehicle, payment, amount, user, desk, exception } = object;
+    const rentalEntityParams: IRental = { _id, client, time, date, vehicle, payment, amount, user, desk, exception };
     const rentalEntity = new RentalEntity(rentalEntityParams);
     return rentalEntity;
   };
