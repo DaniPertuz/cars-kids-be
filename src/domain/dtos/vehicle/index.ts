9bc9961c7ee0c35def4af2f2319692e0dc80fff3
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-04 21:34:13 -0500

    Add rentalInfo field validation

--- src/domain/dtos/vehicle/index.ts
+++ src/domain/dtos/vehicle/index.ts
@@ -1,16 +1,23 @@
 import { IStatus, IVehicle } from '../../../interfaces';
+import { VehicleRentalTime } from '../../../interfaces/index';
 
 export class VehicleDTO {
   private constructor(public params: IVehicle) { }
 
   static create(object: { [key: string]: any; }): [string?, VehicleDTO?] {
-    const { nickname, category, color, size, status = IStatus.Active } = object;
+    const { nickname, category, color, size, rentalInfo, status = IStatus.Active } = object;
 
     if (!nickname) return ['Apodo de vehículo es requerido'];
     if (!category) return ['Categoría de vehículo es requerida'];
     if (!color) return ['Color de vehículo es requerido'];
     if (!size) return ['Tamaño de vehículo es requerido'];
+    if (rentalInfo && Array.isArray(rentalInfo)) {
+      for (const info of rentalInfo) {
+        if (!info.price) return ['Precio de alquiler de vehículo es requerido'];
+        if (!info.time) return ['Tiempo de alquiler de vehículo es requerido'];
+      }
+    }
 
-    return [undefined, new VehicleDTO({ nickname, category, color, size, status })];
+    return [undefined, new VehicleDTO({ nickname, category, color, size, rentalInfo, status })];
   }
 }
